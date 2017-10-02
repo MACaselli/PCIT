22e5c6b0146d1d8704f532f73eccd73ce46913f0
Daniel Serratore <daniel75027@gmail.com>
2017-10-02 00:42:44 -0500

    Changed timer layout to row, replaced control labels with vector icons.

--- src/components/custom/Timer.js
+++ src/components/custom/Timer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/Entypo';
 import { CardSection, Button } from 'common';
 import { timerStart, timerStop, timerReset } from 'actions';
 
@@ -9,15 +10,13 @@ class Timer extends Component{
   render(){
     const { label, value, onChangeText } = this.props;
     var { timer } = this.props;
-    timer = `${Math.floor(timer / 60)}:${(timer % 60 >= 10) ? (timer % 60) : `0${timer % 60}`}`;
+    timer = `${Math.floor(timer / 60)}:${(timer % 60 >= 10) ? (timer % 60) : `0${timer % 60}`}`; // zero-fill
     return(
-    	<CardSection style={{ flexDirection: 'column' }}>
-    		<Text style={{ textAlign: 'center', fontSize: 20, paddingBottom: 10 }}>{timer}</Text>
-    		<View style={{ flexDirection: 'row' }}>
-	    		<Button style={{ flex: 1 }} onPress={this.props.timerStart}>Start</Button>
-	    		<Button style={{ flex: 1 }} onPress={this.props.timerStop}>Stop</Button>
-	    		<Button style={{ flex: 1 }} onPress={this.props.timerReset}>Reset</Button>
-    		</View>
+    	<CardSection style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 0, paddingBottom: 5 }}>
+            <Button style={{ flex: 1 }} onPress={this.props.timerStart}><Icon name="controller-play" size={20} /></Button>
+            <Button style={{ flex: 1 }} onPress={this.props.timerStop}><Icon name="controller-paus" size={20} /></Button>
+            <Button style={{ flex: 1 }} onPress={this.props.timerReset}><Icon name="loop" size={20} /></Button>
+    		<Text style={{ flex: 2, textAlign: 'center', fontSize: 20 }}>{timer}</Text>
     	</CardSection>
     )
   }
